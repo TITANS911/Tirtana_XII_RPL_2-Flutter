ed9dc37fbc7ed8bca3f69ee35e7614c8ae43dde1
TITANS911 <112262438+TITANS911@users.noreply.github.com>
2023-09-18 15:06:20 +0700

    Update main.dart

--- main.dart
+++ main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
       ],
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.teal,
+        primarySwatch: Colors.purple,
       ),
       home: HomeScreen(),
     );
